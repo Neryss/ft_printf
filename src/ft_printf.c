e99d0d426ad51d09cbd082097a396f5b94a74014
Ckurt <kuroysiris@gmail.com>
2020-12-19 17:03:11 +0100

    need to fix precision

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ckurt <ckurt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 13:02:03 by ckurt             #+#    #+#             */
-/*   Updated: 2020/12/19 16:42:43 by ckurt            ###   ########lyon.fr   */
+/*   Updated: 2020/12/19 16:58:28 by ckurt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,7 @@ int	select_parsing(va_list valist, s_element *elem)
 
 	i = 0;
 	// debug_struct(elem);
+			debug_struct(elem);
 	if (elem->type == 'c')
 		return (ft_print_char(elem, valist));
 	if (elem->type == 's')
