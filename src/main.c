030062f48ac964a892b2270fea8467a27f836b87
Ckurt <kuroysiris@gmail.com>
2020-12-22 19:16:06 +0100

    broke some %p, wip %x

--- src/main.c
+++ src/main.c
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[])
 {
 	int r, d = 0;
-	r = ft_printf("0*%-0*.0x*0 ", 21, -1011);
+	r = ft_printf(" --0*%0*.0x*0 0*%0*.10x*0-- ", -2, 0, 21, 1);
 	ft_printf("\n");
 	// ft_printf("%10.21x|", 134);
-	d = printf("0*%-0*.0x*0 ", 21, -1011);
+	d = printf(" --0*%0*.0x*0 0*%0*.10x*0-- ", -2, 0, 21, 1);
 	// printf("%10.21x|", -101);
 	// printf("%10.21x|", 134);
 	printf("\n");
