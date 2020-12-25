7c2551b72db99ce998f86313355b92af80e2b242
Ckurt <kuroysiris@gmail.com>
2020-12-25 13:23:58 +0100

    fuck negatives

--- src/ft_print_di.c
+++ src/ft_print_di.c
@@ -6,15 +6,81 @@
 /*   By: ckurt <ckurt@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 18:48:52 by ckurt             #+#    #+#             */
-/*   Updated: 2020/12/25 12:10:09 by ckurt            ###   ########lyon.fr   */
+/*   Updated: 2020/12/25 13:23:23 by ckurt            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-static int	print_di_justify(t_element *elem, char *str, int len)
+static	int	print_di_else(t_element *elem, char *str, int len)
 {
-	
+	int		i;
+
+	i = 0;
+	if (elem->dot)
+	{
+		if (elem->dot_size > len)
+		{
+			i += print_width(elem, elem->dot_size);
+			i += print_zero(elem->dot_size - len);
+			i += ft_putstrl(str, len);
+		}
+		else
+		{
+			if (len == 1 && str[1] == 0)
+			{
+				i += print_width(elem, elem->dot_size);
+				i += ft_putstrl(str, elem->dot_size);
+			}
+			else
+			{
+				i += print_width(elem, len);
+				i += ft_putstr(str);
+			}
+		}
+	}
+	else
+	{
+		i += print_width(elem, len);
+		i += ft_putstr(str);
+	}
+	return (i);
+}
+
+static	int	print_di_justify(t_element *elem, char *str, int len)
+{
+	int		i;
+
+	i = 0;
+	if (elem->dot)
+	{
+		if (elem->dot_size > len)
+		{
+			i += ft_putstr(str);
+			i += print_zero(elem->dot_size - len);
+			i += print_width(elem, elem->dot_size);
+		}
+		else
+		{
+			if (len == 1 && str[1] == 0)
+			{
+				i += ft_putstrl(str, elem->dot_size);
+				i += print_width(elem, elem->dot_size);
+			}
+			else
+			{
+				i += ft_putstr(str);
+				i += print_width(elem, len);
+			}
+		}
+		
+	}
+	else
+	{
+		i += ft_putstr(str);
+		i += print_width(elem, len);
+	}
+	return (i);
 }
 
 int			ft_print_di(t_element *elem, int nb)
@@ -24,9 +90,11 @@ int			ft_print_di(t_element *elem, int nb)
 	int		len;
 
 	i = 0;
-	(void)elem;
 	str = ft_itoa(nb);
 	len = ft_strlen(str);
-	i += ft_putstr(str);
+	if (elem->left_justify)
+		i += print_di_justify(elem, str, len);
+	else
+		i += print_di_else(elem, str, len);
 	return (i);
 }
--- src/main.c
+++ src/main.c
@@ -5,9 +5,9 @@
 int main(int argc, char *argv[])
 {
 	int r, d = 0;
-	r = ft_printf("|%d|", 2348);
+	r = ft_printf(" %10.42d ", -1);
 	ft_printf("\n");
-	d = printf("|%d|", 2348);
+	d = printf(" %10.42d ", -1);
 
 	printf("\n\nreturn mine is [%d]", r);
 	printf("\n\nreturn libc is [%d]", d);
