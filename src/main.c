102a96586049b3ce66c43d1d643bf68d59c595ca
Ckurt <kuroysiris@gmail.com>
2020-12-28 19:02:36 +0100

    wip patching 42tester

--- src/main.c
+++ src/main.c
@@ -4,12 +4,24 @@
 
 int main(int argc, char *argv[])
 {
-	int r, d = 0;
-	r = ft_printf("%% *.5i 42 == |% *.5i|", 4, 42);
-	ft_printf("\n");
-	d = printf("%% *.5i 42 == |% *.5i|", 4, 42);
+	int		d = 2147483647;
+	char	c = 'a';
+	int		e = -2147483648;
+	int		f = 42;
+	int		g = 25;
+	int		h = 4200;
+	int		i = 8;
+	int		j = -12;
+	int		k = 123456789;
+	int		l = 0;
+	int		m = -12345678;
 
-	printf("\nme  return %d\n", r);
-	printf("lib return %d\n", d);
+	int eheh, oui = 0;
+	eheh = ft_printf("%0.i, %0.d, %0.d, %0.d, %0.d, %0.d, %0.d, %0.d", i, j, k, l, m, c, e, d);
+	ft_printf("\n");
+	oui = printf("%0.i, %0.d, %0.d, %0.d, %0.d, %0.d, %0.d, %0.d", i, j, k, l, m, c, e, d);
+
+	printf("\nme  return %d\n", eheh);
+	printf("lib return %d\n", oui);
 	return (0);
 }
